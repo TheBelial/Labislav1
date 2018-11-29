ea7b4c37b624d64c9b13184a532aeae73f77f332
TheBelial <ruskagame@gmail.com>
2018-11-29 23:46:15 +0200

    mic check 1,2

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percentage_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string sToCheck;
+           // Console.WriteLine("Enter line you want to check:");
+           // sToCheck = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Enter line you want to check:");
+                sToCheck = Console.ReadLine();
+                if (sToCheck!="")
+                {
+                    break;
+                }
+            }
+                
+
+            
+
+            Console.WriteLine("Enter character you want to find in your line:");
+            char c = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            double otvet = CheckForChar(sToCheck, c);
+
+            Console.WriteLine("Your answer is:{0:P1}.",otvet);
+            
+
+            Console.ReadLine();
+            
+
+        }
+
+        static double CheckForChar(string x, char y)
+        {
+            var Pm = x.Length;
+            var Pf = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i].Equals(y))
+                {
+                    Pf++;
+                    
+                }
+               
+            }
+
+          
+
+            
+                double Answ = (double)Pf / Pm;
+            
+             
+            
+            return Answ;
+        }
+    }
+}
